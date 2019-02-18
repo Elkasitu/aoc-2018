5822c32642c3b97123564019baf6766f64f8c72d
Adrian Torres <adt@odoo.com>
2019-02-18 21:29:32 +0100

    day 3 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,78 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::str::Split;
+use std::collections::HashMap;
 
 fn main() {
     println!("Checking for input.txt...");
     let mut input = String::new();
     let mut f = File::open("src/input.txt").unwrap();
     f.read_to_string(&mut input);
-    let barcodes = input.trim().split('\n');
-
-    //println!("The result of the checksum is: {}", calc_checksum(&barcodes));
-    println!("The common letters of the correct barcode are: {:?}", get_common_letters(barcodes));
+    let coords: Vec<&str> = input.trim().split('\n').collect();
+    println!("Day 1 part 1 solution: {}", day_1_part_1(coords));
 }
 
-fn count(needle: &char, haystack: &str) -> i32 {
+struct Plot {
+    id: i32,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+}
+
+impl Plot {
+    fn get_plot_coords(&self) -> Vec<(i32, i32)> {
+        let mut result: Vec<(i32, i32)> = Vec::new();
+        for i in 0..self.width {
+            for j in 0..self.height {
+                result.push((i + self.x, j + self.y));
+            }
+        }
+        return result;
+    }
+}
+
+fn parse_coord(coord: &str, id: i32) -> Plot {
+    let idrest: Vec<&str> = coord.split('@').collect();
+    let startdim: Vec<&str> = idrest[1].trim().split(':').collect();
+    let xy: Vec<&str> = startdim[0].trim().split(',').collect();
+    let x: i32 = xy[0].parse().unwrap();
+    let y: i32 = xy[1].parse().unwrap();
+    let dim: Vec<&str> = startdim[1].trim().split('x').collect();
+    let w: i32 = dim[0].parse().unwrap();
+    let h: i32 = dim[1].parse().unwrap();
+    return Plot { id: id, x: x, y: y, width: w, height: h };
+}
+
+fn parse_coords(coords: Vec<&str>) -> Vec<Plot> {
+    let mut result: Vec<Plot> = Vec::new();
+    for (i, coord) in coords.iter().enumerate() {
+        result.push(parse_coord(coord, (i as i32) + 1));
+    }
+    return result;
+}
+
+fn get_overlaps(plots: Vec<Plot>) -> HashMap<(i32, i32), i32> {
+    let mut overlaps: HashMap<(i32, i32), i32> = HashMap::new();
+    for plot in plots.iter() {
+        let plot_spaces: Vec<(i32, i32)> = plot.get_plot_coords();
+        for space in plot_spaces {
+            *overlaps.entry(space).or_insert(0) += 1;
+        }
+    }
+    return overlaps;
+}
+
+fn get_overlapping_sqm(overlaps: HashMap<(i32, i32), i32>, n: i32) -> i32 {
     let mut i = 0;
-    for c in haystack.chars() {
-        if c == *needle {
+    for v in overlaps.values() {
+        if v >= &n {
             i += 1;
         }
     }
     return i;
 }
 
-fn has_n(code: &str, n: i32) -> bool {
-    for c in code.chars() {
-        if count(&c, &code) == n {
-            return true;
-        }
-    }
-    return false;
-}
-
-fn calc_checksum(barcodes: Split<char>) -> i32 {
-    let mut doubles = 0;
-    let mut triples = 0;
-
-    for barcode in barcodes {
-        if has_n(barcode, 2) {
-            doubles += 1;
-        }
-        if has_n(barcode, 3) {
-            triples += 1;
-        }
-    }
-
-    return doubles * triples;
-}
-
-fn cmp_letters(charset: &Vec<char>, charset2: &Vec<char>) -> (bool, Vec<char>) {
-    let mut res_bool = false;
-    let mut res_vec: Vec<char> = Vec::new();
-    let og_size = charset.len() as i32;
-
-    for (x, y) in charset.iter().zip(charset2.iter()) {
-        if x == y {
-            res_vec.push(*x);
-        }
-    }
-
-    let res_size = res_vec.len() as i32;
-    if og_size == (res_size - 1) {
-        res_bool = true;
-    }
-
-    return (res_bool, res_vec);
-}
-
-fn cmp_words(to_compare: &str, words: Split<char>) -> (bool, Vec<char>) {
-    let charset: Vec<char> = to_compare.chars().collect();
-    let res_vec: Vec<char> = Vec::new();
-
-    for word in words {
-        let charset2: Vec<char> = word.chars().collect();
-        let res = cmp_letters(&charset, &charset2);
-        if res.0 {
-            return res;
-        }
-    }
-    return (false, res_vec);
-}
-
-fn get_common_letters(barcodes: Split<char>) -> Vec<char> {
-    let res_vec: Vec<char> = Vec::new();
-    let barclone = barcodes.clone();
-    for barcode in barcodes {
-        let res = cmp_words(barcode, barclone.clone());
-        if res.0 {
-            return res.1;
-        }
-    }
-    return res_vec;
+fn day_1_part_1(coords: Vec<&str>) -> i32 {
+    let overlaps = get_overlaps(parse_coords(coords));
+    return get_overlapping_sqm(overlaps, 2);
 }
